8c4a4f177b9b1f92d09cb6025ad0fbb45e32d66d
ImmortalityBH <ImmortalityBH@gmail.com>
2020-07-21 21:08:24 -0500

    fixed icon

--- DirectX Dorito Simulator/resource.h
+++ DirectX Dorito Simulator/resource.h
@@ -2,7 +2,7 @@
 // Microsoft Visual C++ generated include file.
 // Used by DirectX Dorito Simulator.rc
 //
-#define IDI_ICON1                       "icon_IOc_icon.ico"
+#define IDI_ICON1                       101
 
 // Next default values for new objects
 // 
